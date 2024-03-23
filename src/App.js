bbd42fecacdbe3917e81a00d112a24c33f5ff6b2
natwarprajapati <140398289+natwarprajapati@users.noreply.github.com>
2024-03-23 11:30:59 +0530

    tops assignment react module 11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Categories from "./compononents/Categories";
+import Header from "./compononents/Header";
+import Slider from "./compononents/Slider";
+import "./Css.scss"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Slider />
+      <Categories />
     </div>
   );
 }
